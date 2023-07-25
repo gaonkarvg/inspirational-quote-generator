a8750c1e43d9c436bfecdba2476cc22afafc6ccc
vignesh <vignesh@graphenesvc.com>
2023-07-25 23:00:11 +0530

    layout and generate quote design adding

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,80 @@
 "use client";
 
-import { GradientBackGroundCon } from "@/components/quoteGenerator/quoteGeneraateorElement";
 import Image from "next/image";
+import React, { useState } from "react";
+
+// Components
+import {
+  BackgroundImage1,
+  BackgroundImage2,
+  FooterCon,
+  GradientBackGroundCon,
+  QuoteGeneratorButton,
+  QuoteGeneratorCon,
+  QuoteGeneratorInnerCon,
+  QuoteGeneratorQuote,
+  QuoteGeneratorQuoteAuthor,
+  QuoteGeneratorQuoteText,
+  QuoteGeneratorSubTitle,
+  QuoteGeneratorTitle,
+} from "@/components/quoteGenerator/quoteGeneraateorElement";
+
+// Assets
+import Clouds from "@/assets/cloud.png";
+import Rain from "@/assets/download_2.png";
 
 export default function Home() {
+  const [quotesGenerated, setQuotesGenerated] = useState<Number>(100);
   return (
+    //  bancground gradient
     <GradientBackGroundCon>
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1 className="text-4xl font-bold text-center">
-          Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
+      {/* Qouote generator Modal Pop-up */}
+      {/* Quote generator Modal */}
+
+      {/* Quote Generator */}
+      <QuoteGeneratorCon>
+        <QuoteGeneratorInnerCon>
+          <QuoteGeneratorTitle>Quote Generator</QuoteGeneratorTitle>
+
+          <QuoteGeneratorSubTitle>
+            {/* A simple app to generate inspirational quotes, */}
+            Generate a quote card with a random inspirational quote provided by
+            ZenQuotes API.
+          </QuoteGeneratorSubTitle>
+
+          <QuoteGeneratorButton
+            onClick={() => setQuotesGenerated(Number(quotesGenerated) + 1)}
+          >
+            Generate Quote
+          </QuoteGeneratorButton>
+
+          <QuoteGeneratorQuote>
+            <QuoteGeneratorQuoteText>
+              "The best way to predict the future is to create it."
+            </QuoteGeneratorQuoteText>
+            <QuoteGeneratorQuoteAuthor> - Abraham Lincoln </QuoteGeneratorQuoteAuthor>
+          </QuoteGeneratorQuote>
+        </QuoteGeneratorInnerCon>
+      </QuoteGeneratorCon>
+
+      {/* background images */}
+      <BackgroundImage1 src={Clouds} height="200" alt="Picture of the author" />
+      <BackgroundImage2 src={Rain} height="200" alt="Picture of the author" />
+
+      {/* footer */}
+      <FooterCon>
+        <>
+          Quotes Generated: {quotesGenerated} <br />
+          Developed by:{" "}
+          <a
+            href="https://www.linkedin.com/in/gaonkarvg/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vigneshwar Gaonkar
           </a>
-        </h1>
-      </main>
+        </>
+      </FooterCon>
     </GradientBackGroundCon>
   );
 }
--- components/quoteGenerator/quoteGeneraateorElement.tsx
+++ components/quoteGenerator/quoteGeneraateorElement.tsx
@@ -1,22 +1,173 @@
-import styled  from "styled-components";
+import styled from "styled-components";
 import Link from "next/link";
 import Image from "next/image";
 
 export const GradientBackGroundCon = styled.div`
-    background: linear-gradient(to right, #000046, #1cb5e0);
-    background-size: 400% 400%;
-    animation: gradient 15s ease infinite;
-    height: 100vh;
-    width: 100vw;
-    @keyframes gradient {
-        0% {
-            background-position: 0% 50%;
-        }
-        50% {
-            background-position: 100% 50%;
-        }
-        100% {
-            background-position: 0% 50%;
-        }
+  background: linear-gradient(to right, #000046, #1cb5e0);
+  background-size: 400% 400%;
+  animation: gradient 15s ease infinite;
+  height: 100vh;
+  width: 100vw;
+  @keyframes gradient {
+    0% {
+      background-position: 0% 50%;
     }
+    50% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0% 50%;
+    }
+  }
+`;
+
+export const BackgroundImage1 = styled(Image)`
+  position: relative;
+  z-index: 1;
+  margin-left: -42px;
+  margin-top: -10px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+  position: fixed;
+  z-index: 1;
+  right: 25px;
+  bottom: 30px;
+`;
+
+export const FooterCon = styled.div`
+  width: 100vw;
+  height: 50px;
+  text-align: center;
+  font-size: 12px;
+  font-weight: 600;
+  position: absolute;
+  z-index: 999;
+  position: fixed;
+  bottom: 0;
+  color: white;
+  a {
+    color: #fff;
+    text-decoration: none;
+  }
+`;
+
+export const QuoteGeneratorCon = styled.div`
+  min-height: 350px;
+  min-width: 350px;
+  height: 70vh;
+  width: 70vw;
+  border: 2px solid #ffffff22;
+  border-radius: 15px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  z-index: 2;
+
+  background: rgba(0, 0, 70, 0.5);
+  box-shadow: 0 8px 32px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  border-radius: 10px;
+  border: 1px solid rgba(255, 255, 255, 0.18);
+`;
+
+export const QuoteGeneratorInnerCon = styled.div`
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  position: absolute;
+  width: 100%;
+  background-color: #fff;
+
+  width: 500px;
+  height: 500px;
+  background-color: #fff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const QuoteGeneratorTitle = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  color: #000;
+`;
+
+export const QuoteGeneratorSubTitle = styled.h2`
+  font-size: 20px;
+  font-weight: 600;
+  color: #000;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+export const QuoteGeneratorButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: #000;
+  color: #fff;
+  border: none;
+  border-radius: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #c0c0c0;
+    color: #000;
+  }
+`;
+
+export const QuoteGeneratorQuote = styled.div`
+  width: 400px;
+  height: 200px;
+  background-color: #000;
+  color: #fff;
+  border-radius: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const QuoteGeneratorQuoteText = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  color: #fff;
+  text-align: center;
+`;
+
+export const QuoteGeneratorQuoteAuthor = styled.div`
+  font-size: 20px;
+  font-weight: 600;
+  color: #fff;
+  text-align: center;
+  margin-top: 10px;
+`;
+
+export const QuoteGeneratorQuoteButton = styled.button`
+  width: 200px;
+  height: 50px;
+  background-color: #fff;
+  color: #000;
+  border: none;
+  border-radius: 10px;
+  font-size: 20px;
+  font-weight: 600;
+  margin-top: 20px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+  &:hover {
+    background-color: #000;
+    color: #fff;
+  }
 `;
